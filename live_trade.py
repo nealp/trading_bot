9b61373a64bea98b702f010d01f8c79228d1b943
Neal Patel <nealp3034@gmail.com>
2025-01-23 22:38:36 -0500

    Remove unused import, fix bug regarding strategy
    Fix bug in strategy where wrong parameter used in add_strtegy attribute

--- live_trade.py
+++ live_trade.py
@@ -1,9 +1,9 @@
-from tradingbot import MLTrader, strategy, API_KEY,API_SECRET,BASE_URL
-from lumibot.credentials import IS_BACKTESTING
-from lumibot.traders import Trader # for  live deployment
+from tradingbot import MLTrader, strategy
+from lumibot.credentials import IS_BACKTESTING #might use later
+from lumibot.traders import Trader # for live deployment
 
 
 if __name__ == 'main':
       trader= Trader() #create variable to host our live trading object
-      trader.add_strategy(MLTrader) #import our MLTrader strategy 
+      trader.add_strategy(strategy=strategy) #import our MLTrader strategy 
       trader.run_all() #runs the strategy live 
